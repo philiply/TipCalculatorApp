8fe1f5b38feafd96d250d558c2276efcc51f9efe
Philip Ly <philiply@berkeley.edu>
2014-08-14 20:05:39 -0700

    added tip calculation functionality and icon
    15, 20, and 25% can now be calculated. icons of different res committed
    as well

--- app/src/main/java/com/philiply/tipcalculator/MainActivity.java
+++ app/src/main/java/com/philiply/tipcalculator/MainActivity.java
@@ -1,9 +1,18 @@
 package com.philiply.tipcalculator;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.NumberFormat;
 
 
 public class MainActivity extends Activity {
@@ -33,4 +42,37 @@ public class MainActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    public void calculateTip(View v) {
+        float tipPerc = Float.parseFloat(v.getTag().toString());
+
+        EditText subtotalInput = (EditText) findViewById(R.id.subtotal_input);
+
+        if (!subtotalInput.getText().toString().equals("")) {
+            float subtotalAmount = Float.parseFloat(subtotalInput.getText().toString());
+            TextView tipTextView = (TextView) findViewById(R.id.tip_amount_value);
+            TextView totalTextView = (TextView) findViewById(R.id.final_total);
+            NumberFormat formatter = NumberFormat.getCurrencyInstance();
+
+            float tipAmount = subtotalAmount * tipPerc;
+
+            float total = subtotalAmount + tipAmount;
+
+            tipTextView.setText(formatter.format(tipAmount));
+            totalTextView.setText(formatter.format(total));
+        } else {
+            makeToast("Empty subtotal");
+        }
+
+
+
+
+
+    }
+
+    private void makeToast(String text) {
+        Toast toast = Toast.makeText(getApplicationContext(),text,Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
 }
